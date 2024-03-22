7300f9e2f3ef87c557a021e98f9e0724972eee2b
Priyansh Sangwan <113136849+priyansh404@users.noreply.github.com>
2024-03-22 20:40:08 +0530

    Updated Solution - GfG to GitHub

--- Easy/Boundary traversal of matrix/boundary-traversal-of-matrix.cpp
+++ Easy/Boundary traversal of matrix/boundary-traversal-of-matrix.cpp
@@ -11,16 +11,20 @@ class Solution
     //traversal of the matrix in a clockwise manner.
     vector<int> boundaryTraversal(vector<vector<int> > matrix, int n, int m) 
     {
-    vector<int> result;
-    for(int i=0; i<m; i++)
-        result.push_back(matrix[0][i]);
-    for(int i=1; i<n; i++)
-        result.push_back(matrix[i][m-1]);
-    for(int i=m-2; i>=0 && n>1; i--)
-        result.push_back(matrix[n-1][i]);
-    for(int i=n-2; i>=1 && m > 1; i--)
-        result.push_back(matrix[i][0]);
-    return result;
+        vector<int> ans;
+        for(int i = 0; i < m; i++){
+            ans.push_back(matrix[0][i]);
+        }
+        for(int i = 1; i < n; i++){
+            ans.push_back(matrix[i][m-1]);
+        }
+        for(int i = m-2; i >= 0 && n > 1; i--){
+            ans.push_back(matrix[n-1][i]);
+        }
+        for(int i = n-2; i >= 1 && m > 1; i--){
+            ans.push_back(matrix[i][0]);
+        }
+        return ans;
     }
 };
 
